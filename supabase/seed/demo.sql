e73d20c1cc906e370855b343982e45ff3f79a99b
carnespatagoniaf-jpg <carnespatagoniaf@gmail.com>
2026-07-13 15:09:56 -0300

    fix npm workspaces v3.1.2

--- supabase/seed/demo.sql
+++
@@ -1,9 +0,0 @@
-insert into public.companies (id, name)
-values ('11111111-1111-1111-1111-111111111111', 'Carnes Patagonia')
-on conflict do nothing;
-
-insert into public.branches (id, company_id, name)
-values
-('22222222-2222-2222-2222-222222222221', '11111111-1111-1111-1111-111111111111', 'Haedo'),
-('22222222-2222-2222-2222-222222222222', '11111111-1111-1111-1111-111111111111', 'Castelar')
-on conflict do nothing;
